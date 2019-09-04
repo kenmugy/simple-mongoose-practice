ef67f4b7f4b2d0b3ab82f916f47a522de3e370de
ken <kenmugy@gmail.com>
2019-09-04 14:11:13 +0300

    initialized app also created create course functionality

---
+++ index.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+
+mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true });
+
+mongoose.connection
+  .then(() => console.log('connected to database.........'))
+  .catch(err => console.log('error connected to mongodb', err));
+
+const courseSchema = new Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
+
+const Course = model('playground', courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: 'Angular.js course',
+    author: 'kenmugy',
+    tags: ['angular', 'frontend'],
+    isPublished: false
+  });
+  const result = await course.save();
+  console.log(result);
+}
+
+createCourse();
